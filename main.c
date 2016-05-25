406e43e5808b7ee935e53b216fdce0b94e317036
wzkDTerH <694994343@qq.com>
2016-05-25 20:58:12 +0800

    rebuild project

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#include "shell_cd.h"
 
 int main()
 {
---
+++ shell_cd.h
@@ -0,0 +1,4 @@
+int shell_cd()
+{
+    return 0;
+}
