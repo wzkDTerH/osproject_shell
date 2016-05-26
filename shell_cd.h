a6b66fa3199a439e7bc219817217ce656283c67c
wzkDTerH <694994343@qq.com>
2016-05-27 00:16:39 +0800

    fix cd bug <cd>

--- shell_cd.h
+++ shell_cd.h
@@ -4,13 +4,17 @@ int shell_cd(char *args[])
 {
 	char path_tmp[BUFSIZE];
 	memset(path_tmp,0,sizeof(path_tmp));
-	if(args[1]==NULL) return 0;
-	if(args[1][0]!='/' && args[1][0]!='.')
+	if(args[1]!=NULL)
 	{
-		strcpy(path_tmp,pathname);
-		strcat(path_tmp,"/");
+		if(args[1][0]!='/' && args[1][0]!='.')
+		{
+			strcpy(path_tmp,pathname);
+			strcat(path_tmp,"/");
+		}
+		strcat(path_tmp,args[1]);
 	}
-	strcat(path_tmp,args[1]);
+	else
+		strcpy(path_tmp,usrpsw->pw_dir);
 	if(chdir(path_tmp)==-1)
 		CmdFail(args,"no such file or directory");
 	return 0;
