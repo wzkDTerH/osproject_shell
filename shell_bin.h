81d7e28df1b66b818c65c08ae8520d1382923d6e
wzkDTerH <694994343@qq.com>
2016-05-27 00:08:00 +0800

    Add bin (bug<display>)

--- shell_bin.h
+++ shell_bin.h
@@ -4,9 +4,15 @@
 int shell_bin(char *arg[])
 {
 	char cmd_tmp[BUFSIZE]="/bin/";
+	int pid;
 	strcat(cmd_tmp,arg[0]);
-	if(execvp(cmd_tmp,arg)==-1)
-		CmdFail(arg,"Command not found!");
+	if((pid=fork())<0)	Err("Fork Error");
+	if(pid==0)
+	{
+		if(execv(cmd_tmp,arg)==-1)
+			CmdFail(arg,"Command not found!");
+		exit(0);
+	}
 	return 0;
 }
 
